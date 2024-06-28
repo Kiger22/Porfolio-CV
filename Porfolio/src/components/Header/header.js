6ef4dac01eded341885a2794e4e687213dab6602
Kiger22 <143103592+Kiger22@users.noreply.github.com>
2024-06-28 16:30:23 +0200

    Create header.js

---
+++ Porfolio/src/components/Header/header.js
@@ -0,0 +1,52 @@
+import "./header.css"
+
+export const createHeader = (logoSrc, menuItems, menuItemsII) => {
+  const header = document.createElement("header");
+  header.className = "header";
+  document.body.appendChild(header);
+
+  // logoSrc
+  const logoSection = document.createElement("div");
+  logoSection.className = "header-logo";
+  const logoImg = document.createElement("img");
+  logoImg.src = logoSrc;
+  logoSection.appendChild(logoImg);
+  header.appendChild(logoSection);
+
+
+
+  // menuItems
+  const menuSection = document.createElement("nav");
+  menuSection.className = "header-menu";
+  menuItems.forEach(item => {
+    const link = document.createElement("a");
+    link.href = item.href;
+    link.innerText = item.text;
+    menuSection.appendChild(link);
+  });
+  header.appendChild(menuSection);
+
+  // menuItemsII
+  if (!menuItemsII) {
+    const menuSectionII = document.createElement("nav");
+    menuSectionII.className = "header-menu-login";
+    menuItemsII.forEach(item => {
+      const link = document.createElement("a");
+      link.href = item.href;
+      link.innerText = item.text;
+      menuSectionII.appendChild(link);
+    });
+    header.appendChild(menuSectionII);
+  }
+
+  /* // loginSeccion
+  const loginSection = document.createElement("div");
+  loginSection.className = "header-login";
+  createButton(loginSection, "Iniciar Seccion", OnLogin);
+  createButton(loginSection, " Registrarse", OnRegister);
+
+  header.appendChild(loginSection); */
+};
+
+
+
