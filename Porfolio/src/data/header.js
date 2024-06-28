1579719deebc45e359c3604b59b092318dbe064f
Kiger22 <143103592+Kiger22@users.noreply.github.com>
2024-06-28 16:30:25 +0200

    Create header.js

---
+++ Porfolio/src/data/header.js
@@ -0,0 +1,13 @@
+export const menuItemsHeaderI = [
+  { text: "Home", href: '#' },
+  { text: "About", href: '#' },
+  { text: "Skills", href: '#' },
+  { text: "Blogs", href: '#' },
+  { text: "Contact", href: '#' },
+];
+
+export const menuItemsHeaderII = [
+
+];
+
+export const heaterLogo = "./public/assets/Logo.png"
