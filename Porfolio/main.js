529bb5bc90611c54eedba23c8bb072dd33471b68
Kiger22 <143103592+Kiger22@users.noreply.github.com>
2024-06-28 16:29:26 +0200

    Update main.js

--- Porfolio/main.js
+++ Porfolio/main.js
@@ -1,1 +1,5 @@
+import { createHeader } from './src/components/Header/header'
+import { heaterLogo, menuItemsHeaderI, menuItemsHeaderII } from './src/data/header'
 import './style.css'
+
+createHeader(heaterLogo, menuItemsHeaderI)
