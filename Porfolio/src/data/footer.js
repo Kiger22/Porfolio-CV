d11f4a2185c0fbd685abcdb1e827bc137219c00f
Kiger22 <143103592+Kiger22@users.noreply.github.com>
2024-07-05 00:42:10 +0200

    Create footer.js

---
+++ Porfolio/src/data/footer.js
@@ -0,0 +1,14 @@
+export const footerLogo = "./public/assets/LogoSolo.png"
+
+export const menuFooter = [
+  { text: 'Inicio', href: '#' },
+  { text: 'Curriculum Vitae', href: '#' },
+  { text: 'Blog', href: '#' },
+  { text: 'RRSS', href: '#' }
+];
+
+export const socialLinks = [
+  { href: 'https://facebook.com', src: "./public/assets/facebook.png" },
+  { href: 'https://twitter.com', src: "./public/assets/twitter.png" },
+  { href: 'https://instagram.com', src: "./public/assets/instagram.png" }
+];
