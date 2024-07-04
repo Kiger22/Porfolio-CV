ac2558ba486f7b3072f64d4f2624b2bd6897bec4
Kiger22 <143103592+Kiger22@users.noreply.github.com>
2024-07-05 00:41:03 +0200

    Update main.js

--- Porfolio/main.js
+++ Porfolio/main.js
@@ -1,5 +1,19 @@
+import { createEducation } from './src/components/Education/education';
+import { createExperience } from './src/components/Experience/experience';
+import { createFooter } from './src/components/Footer/footer';
 import { createHeader } from './src/components/Header/header'
+import { createHero } from './src/components/Hero/hero';
+import { createSkills } from './src/components/Skills/skills';
+import { DATA } from './src/data/data';
+import { footerLogo, menuFooter, socialLinks } from './src/data/footer';
 import { heaterLogo, menuItemsHeaderI, menuItemsHeaderII } from './src/data/header'
 import './style.css'
 
-createHeader(heaterLogo, menuItemsHeaderI)
+const divApp = document.querySelector("#app");
+
+createHeader(heaterLogo, menuItemsHeaderI, menuItemsHeaderII);
+createHero(divApp, DATA.title);
+createSkills(divApp, DATA.skills);
+createEducation(divApp, DATA.education);
+createExperience(divApp, DATA.experience);
+createFooter(footerLogo, menuFooter, socialLinks);
