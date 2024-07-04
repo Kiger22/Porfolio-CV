f31c97c2fdce820a953d80edc6246f01744140ba
Kiger22 <143103592+Kiger22@users.noreply.github.com>
2024-07-05 00:42:08 +0200

    Create data.js

---
+++ Porfolio/src/data/data.js
@@ -0,0 +1,54 @@
+export const DATA = {
+
+  title: {
+    name: "Guillermo Mendoza",
+    title: "Desarrollador Web",
+    contactInfo: "kiger22@hotmail.com",
+    photoUrl: "./public/assets/LogoSolo.png"
+  },
+
+  experience: [
+    {
+      jobTitle: "Desarrollador Frontend",
+      company: "Tech Solutions",
+      dates: "Enero 2020 - Presente",
+      description: "Desarrollo de aplicaciones web ."
+    },
+    {
+      jobTitle: "Desarrollador Frontend",
+      company: "Tech Solutions",
+      dates: "Enero 2020 - Presente",
+      description: "Desarrollo de aplicaciones web ."
+    }
+
+  ],
+
+  education: [
+    {
+      degree: "Licenciatura en Ingeniería Informática",
+      school: "Universidad de Barcelona",
+      dates: "2015 - 2019",
+      description: "Desarrollo de aplicaciones web ."
+    },
+    {
+      degree: "Licenciatura en Ingeniería Informática",
+      school: "Universidad de Barcelona",
+      dates: "2015 - 2019",
+      description: "Desarrollo de aplicaciones web ."
+    },
+    {
+      degree: "Licenciatura en Ingeniería Informática",
+      school: "Universidad de Barcelona",
+      dates: "2015 - 2019",
+      description: "Desarrollo de aplicaciones web ."
+    }
+  ],
+
+  skills: [
+    "HTML5",
+    "CSS",
+    "JavaScript",
+    "Vite",
+
+  ]
+};
