7bb83a51da7c37102b95bb7d63cd449b6ac9e07a
Kiger22 <143103592+Kiger22@users.noreply.github.com>
2024-07-12 18:25:27 +0200

    Update education.js

--- Porfolio/src/components/Education/education.js
+++ Porfolio/src/components/Education/education.js
@@ -1,25 +1,51 @@
+import("./education.css")
+
 export const createEducation = (node, EDUCATION) => {
 
   const education = document.createElement("section");
   education.className = "section-education";
+  education.id = "Education"
   node.appendChild(education);
 
-  EDUCATION.forEach(({ degree, school, dates, description }) => {
+  const educationTitle = document.createElement("h1");
+  educationTitle.textContent = "Formacion";
+  educationTitle.className = "education-title";
+  education.appendChild(educationTitle);
+
+  const educationGrid = document.createElement("div");
+  educationGrid.className = "education-grid";
+  education.appendChild(educationGrid);
+
+  EDUCATION.forEach(({ degree, school, dates, description, logo }) => {
+
+    const educationItem = document.createElement("div");
+    educationItem.className = "education-item";
+    educationGrid.appendChild(educationItem);
+
+    const logoElement = document.createElement("img");
+    logoElement.src = logo;
+    logoElement.className = "education-logo";
+    educationItem.appendChild(logoElement);
+
     const degreeElement = document.createElement("h3");
     degreeElement.textContent = degree;
-    education.appendChild(degreeElement);
+    degreeElement.className = "degree";
+    educationItem.appendChild(degreeElement);
 
     const schoolElement = document.createElement("h4");
     schoolElement.textContent = school;
-    education.appendChild(schoolElement);
+    schoolElement.className = "school";
+    educationItem.appendChild(schoolElement);
 
     const datesElement = document.createElement('p');
     datesElement.textContent = dates;
-    education.appendChild(datesElement);
+    datesElement.className = "dates-education";
+    educationItem.appendChild(datesElement);
 
     const descriptionElement = document.createElement("p");
-    datesElement.textContent = description;
-    education.appendChild(descriptionElement);
+    descriptionElement.textContent = description;
+    descriptionElement.className = "description-education";
+    educationItem.appendChild(descriptionElement);
 
   });
 
