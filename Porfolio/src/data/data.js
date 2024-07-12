eb6737316c2f9d0216ebee2e66c2d8eec9654a25
Kiger22 <143103592+Kiger22@users.noreply.github.com>
2024-07-12 18:25:58 +0200

    Update data.js

--- Porfolio/src/data/data.js
+++ Porfolio/src/data/data.js
@@ -1,10 +1,11 @@
 export const DATA = {
 
-  title: {
+  aboutMe: {
+    avatar: "./assets/myAvatar.png",
     name: "Guillermo Mendoza",
-    title: "Desarrollador Web",
-    contactInfo: "kiger22@hotmail.com",
-    photoUrl: "./public/assets/LogoSolo.png"
+    aboutMe: "Desarrollador web",
+    address: "L'Ampolla, Tarragona (España)",
+    email: "kiger22@hotmail.com",
   },
 
   experience: [
@@ -12,43 +13,82 @@ export const DATA = {
       jobTitle: "Desarrollador Frontend",
       company: "Tech Solutions",
       dates: "Enero 2020 - Presente",
-      description: "Desarrollo de aplicaciones web ."
+      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad, dolor ratione! Eaque delectus voluptas rerum, eligendi tempora voluptatum cupiditate enim recusandae eos vitae velit fuga harum ea expedita aperiam blanditiis?"
     },
     {
       jobTitle: "Desarrollador Frontend",
       company: "Tech Solutions",
       dates: "Enero 2020 - Presente",
-      description: "Desarrollo de aplicaciones web ."
+      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad, dolor ratione! Eaque delectus voluptas rerum, eligendi tempora voluptatum cupiditate enim recusandae eos vitae velit fuga harum ea expedita aperiam blanditiis?."
+    },
+    {
+      jobTitle: "Desarrollador Frontend",
+      company: "Tech Solutions",
+      dates: "Enero 2020 - Presente",
+      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad, dolor ratione! Eaque delectus voluptas rerum, eligendi tempora voluptatum cupiditate enim recusandae eos vitae velit fuga harum ea expedita aperiam blanditiis? ."
+    },
+    {
+      jobTitle: "Desarrollador Frontend",
+      company: "Tech Solutions",
+      dates: "Enero 2020 - Presente",
+      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad, dolor ratione! Eaque delectus voluptas rerum, eligendi tempora voluptatum cupiditate enim recusandae eos vitae velit fuga harum ea expedita aperiam blanditiis?."
+    },
+    {
+      jobTitle: "Desarrollador Frontend",
+      company: "Tech Solutions",
+      dates: "Enero 2020 - Presente",
+      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad, dolor ratione! Eaque delectus voluptas rerum, eligendi tempora voluptatum cupiditate enim recusandae eos vitae velit fuga harum ea expedita aperiam blanditiis?"
     }
 
   ],
 
   education: [
     {
-      degree: "Licenciatura en Ingeniería Informática",
-      school: "Universidad de Barcelona",
+      degree: "Bachelor of Science in Computer Science",
+      school: "University A",
       dates: "2015 - 2019",
-      description: "Desarrollo de aplicaciones web ."
+      description: "Focused on software engineering and data science.",
+      logo: "./assets/University1.png"
+    },
+    {
+      degree: "Master of Science in Artificial Intelligence",
+      school: "University B",
+      dates: "2019 - 2021",
+      description: "Specialized in machine learning and neural networks.",
+      logo: "./assets/University2.png"
+    },
+    {
+      degree: "PhD in Computer Science",
+      school: "University C",
+      dates: "2021 - 2025",
+      description: "Research on quantum computing and cryptography.",
+      logo: "./assets/University3.png"
     },
     {
       degree: "Licenciatura en Ingeniería Informática",
       school: "Universidad de Barcelona",
       dates: "2015 - 2019",
-      description: "Desarrollo de aplicaciones web ."
+      description: "Desarrollo de aplicaciones web .",
+      logo: "./assets/University4.png"
     },
     {
       degree: "Licenciatura en Ingeniería Informática",
       school: "Universidad de Barcelona",
       dates: "2015 - 2019",
-      description: "Desarrollo de aplicaciones web ."
+      description: "Desarrollo de aplicaciones web .",
+      logo: "./assets/University4.png"
     }
   ],
 
   skills: [
-    "HTML5",
-    "CSS",
-    "JavaScript",
-    "Vite",
-
+    "./assets/HTMLLogo.png",
+    "./assets/CSS3Logo.png",
+    "./assets/javascriptLogo.png",
+    "./assets/ViteLogo.png",
+    "./assets/GitHubLogo.png",
+    "./assets/JavaLogo.png",
+    "./assets/ReactLogo.png",
+    "./assets/PythonLogo.png"
   ]
 };
+
