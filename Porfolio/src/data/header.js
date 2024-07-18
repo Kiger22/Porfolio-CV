be6e5b98ee886e4d96fe731183e4f356de4d64e3
Kiger22 <143103592+Kiger22@users.noreply.github.com>
2024-07-18 13:17:46 +0200

    Update header.js

--- Porfolio/src/data/header.js
+++ Porfolio/src/data/header.js
@@ -1,13 +1,22 @@
+export const meniItemsMenuToggle = [
+  { href: '#', imgSrc: "./assets/About.svg", alt: 'Home', text: 'About Me' },
+  { href: '#Experience', imgSrc: "./assets/Experience.svg", alt: 'Experience', text: 'Experiencia' },
+  { href: '#Education', imgSrc: "./assets/Education.svg", alt: 'Education', text: 'Educacion' },
+  { href: '#Contact', imgSrc: "./assets/contact.svg", alt: 'Contact Me', text: 'Contacto' },
+  { href: '#', imgSrc: "./assets/menu.svg", alt: 'Menu' }
+];
+
 export const menuItemsHeaderI = [
-  { text: "About Me", href: '#' },
-  { text: "Experience", href: '#Experience' },
-  { text: "Education", href: '#Education' },
-  { text: "Skills", href: '#' },
-  { text: "Contact", href: '#' },
+  { text: "Inicio", href: '#' },
+  { text: "Experiencia", href: '#Experience' },
+  { text: "Formacion", href: '#Education' },
+  { text: "Skills", href: '#Skills' },
+  { text: "Proyectos", href: '#Projects' },
+  { text: "Contacto", href: '#Contact' },
 ];
 
 export const menuItemsHeaderII = [
 
 ];
 
-export const heaterLogo = "./assets/Logo.png"
+export const heaterLogo = "./assets/LogoSolo.png"
