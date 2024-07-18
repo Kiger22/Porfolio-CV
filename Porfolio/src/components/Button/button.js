eb211951fa90f0e0dd5f0df30aba2e45fb49843e
Kiger22 <143103592+Kiger22@users.noreply.github.com>
2024-07-18 13:16:46 +0200

    Update button.js

--- Porfolio/src/components/Button/button.js
+++ Porfolio/src/components/Button/button.js
@@ -4,6 +4,7 @@ export const createButton = (node, text, onClick) => {
   const button = document.createElement("button");
   button.className = "button";
   button.innerText = text;
+  button.type = "submit";
   node.append(button);
   button.addEventListener("click", onClick);
 };
