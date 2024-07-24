8ffdc5a32c8481500c3c4bede9f04824027ae9cb
Kiger22 <143103592+Kiger22@users.noreply.github.com>
2024-07-24 02:00:59 +0200

    Update main.js

--- Porfolio/main.js
+++ Porfolio/main.js
@@ -1,30 +1,29 @@
 import { toggletheme } from './actions/toggleTheme';
 
-import { crearContactSection } from './src/components/Contact/contact';
-import { createEducation } from './src/components/Education/education';
-import { createExperienceSection } from './src/components/Experience/experience';
 import { createFooter } from './src/components/Footer/footer';
 import { createHeader } from './src/components/Header/header'
-import { createHeroSection } from './src/components/Hero/hero';
-import { createMenuToggle, menuToggle } from './src/components/MenuToggle/menuToggle';
-import { createProjectSection } from './src/components/Projects/projects';
-import { createSkillsSection } from './src/components/Skills/skills';
+import { createMenuToggle } from './src/components/MenuToggle/menuToggle';
 import { createSwitchButton } from './src/components/SwitchButton/switchButton';
 import { DATA } from './src/data/data';
 import { footerLogo, menuFooter, socialLinks } from './src/data/footer';
 import { heaterLogo, meniItemsMenuToggle, menuItemsHeaderI, menuItemsHeaderII } from './src/data/header'
+import { createEducation } from './src/pages/Education/education';
+import { createExperienceSection } from './src/pages/Experience/experience';
+import { createHeroPage } from './src/pages/Hero/hero';
+import { createProjectSection } from './src/pages/Projects/projects';
+import { createSkillsSection } from './src/pages/Skills/skills';
 import './style.css'
 
 const divApp = document.querySelector("#app");
 
 createHeader(heaterLogo, menuItemsHeaderI, menuItemsHeaderII);
 
-createHeroSection(divApp, DATA.aboutMe);
+// createHeroPage(divApp, DATA.hero);
+//createExperienceSection(divApp, DATA.experience);
+//createEducation(divApp, DATA.education);
+//createProjectSection(divApp, DATA.projects);
 createSkillsSection(divApp, DATA.skills);
-createEducation(divApp, DATA.education);
-createExperienceSection(divApp, DATA.experience);
-createProjectSection(divApp, DATA.projects);
-crearContactSection(divApp);
+
 createFooter(footerLogo, menuFooter, socialLinks);
 
 createSwitchButton(divApp);
