927d15fe4b53422a0f325962a074631fce5a324d
Kiger22 <143103592+Kiger22@users.noreply.github.com>
2024-07-25 10:15:59 +0200

    Update switchButton.js

--- Porfolio/src/components/SwitchButton/switchButton.js
+++ Porfolio/src/components/SwitchButton/switchButton.js
@@ -1,12 +1,21 @@
 import("./switchButton.css")
 
-export const createSwitchButton = (node) => {
-  node.innerHTML += ` <div class="switch-conteiner">
-                        <div id="switch">
-                          <div id="circle"></div>
-                        </div>
-                      </div>  `;
+export const SwitchButton = () => {
+
+  const switchContainer = document.createElement('div');
+  switchContainer.className = 'switch-container';
+
+  const switchElement = document.createElement('div');
+  switchElement.id = 'switch';
+  switchContainer.appendChild(switchElement);
+
+  const circle = document.createElement('div');
+  circle.id = 'circle';
+  switchElement.appendChild(circle);
+
+  document.body.appendChild(switchContainer);
 };
+
 
 
 
